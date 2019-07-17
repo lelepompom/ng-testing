2a0387d39194733bce2c8273679e04d84dda9a73
formacion <formacion@ANSFORM33.indra.es>
2019-07-17 11:51:10 +0200

    Test default spec and add Karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-testing'`, () => {
+  it(`should have as title 'Testing with Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-testing');
+    expect(app.title).toEqual('Testing with Angular');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-testing!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Testing with Angular!');
   });
 });
